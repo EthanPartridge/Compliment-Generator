37584a3ab99bd999e73c43f73597f77ca77686ae
EthanPartridge98 <eepartridg@eagles.usi.edu>
2021-07-07 18:04:45 -0500

    First Commit

---
+++ ComplimentGeneratorConsoleApp/Compliment.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComplimentGeneratorConsoleApp
+{
+    class Compliment
+    {
+        private List<string> AdjectiveList = new List<string> 
+        {
+            "Beautiful",
+            "Perfect",
+            "Lovely",
+            "Nice",
+            "Cool",
+            "Awesome",
+            "Epic",
+            "Radical",
+            "Gnarly",
+            "Sick",
+            "Amazing",
+            "Wonderful",
+            "Sweet",
+            "Good",
+            "Great",
+            "Incredible",
+            "Huge",
+            "Big",
+            "Massive",
+            "Absolute",
+            "Astounding",
+            "Encapsulating",
+            "Pretty",
+            "Cute",
+            "Likable",
+            "Marvelous",
+            "Excellent",
+            "Outstanding",
+            "Unique",
+            "Remarkable",
+            "Spectacular",
+            "Super",
+            "Stellar",
+            "Stunning",
+            "Vibrant",
+            "Elegant",
+            "Brilliant",
+            "Glowing",
+            "Captivating",
+            "Enchanting",
+            "Magnificent",
+            "Charming",
+            "Dream",
+            "Genuine",
+            "Harmonious",
+            "Inspirational",
+            "Shining",
+            "Clever",
+            "Effortless",
+            "Graceful",
+            "Legendary",
+            "Wholesome",
+            "Adored",
+            "Brilliant",
+            "Dazzling",
+            "Electrifying",
+            "Exquisite",
+            "Splendid"
+        };
+        private List<string> NounList = new List<string> 
+        { 
+            "Angel",
+            "Sweetie",
+            "Pumpklin", 
+            "Baby",
+            "Being",
+            "Human",
+            "Work-of-art",
+            "Boss",
+            "King",
+            "Queen",
+            "Charmer",
+            "Dream",
+            "Flame",
+            "Spark",
+            "Icon",
+            "Individual",
+            "Person",
+            "Jewel",
+            "Gem",
+            "Heartthrob",
+            "Inspiration",
+            "Legend",
+            "Pal",
+            "Friend",
+            "Superstar",
+            "Sweetheart",
+            "Light",
+            "Miracle",
+            "Star",
+            "Soul",
+            "Character",
+            "Marvel",
+            "Champion"
+        };
+
+        public void RandomCompliment()
+        {
+            Random rnd = new Random();
+            int rA = rnd.Next(AdjectiveList.Count);
+            int rN = rnd.Next(NounList.Count);
+            string rI = $"\n{(string)AdjectiveList[rA]} {(string)NounList[rN]}";
+
+            foreach (char c in rI) { Console.Write("-"); }
+            Console.WriteLine(rI);
+            foreach (char c in rI) { Console.Write("-"); }
+            Console.WriteLine("\n");
+            return;
+        }
+
+        public void NameCompliment(string name)
+        {
+            Int32.TryParse(DetermineCharacterValue(name.ToUpper()), out int nameValue);
+            int nA = nameValue;
+            int nN = nameValue;
+
+            while (nA > AdjectiveList.Count())
+            {
+                nA -= AdjectiveList.Count();
+            }
+            while (nN > NounList.Count())
+            {
+                nN -= NounList.Count();
+            }
+
+            string nI = $"\n{(string)AdjectiveList[nA]} {(string)NounList[nN]}";
+
+            foreach (char c in nI) { Console.Write("-"); }
+            Console.WriteLine(nI);
+            foreach (char c in nI) { Console.Write("-"); }
+            Console.WriteLine("\n");
+            return;
+        }
+
+        public void BirthdayCompliment(string birthday)
+        {
+            string[] sArray = birthday.Split("/");
+            int bA = 0;
+            int bN = 0;
+
+            foreach (string s in sArray)
+            {
+                Int32.TryParse(s, out int birthdayValue);
+                bA += birthdayValue;
+                bN += birthdayValue;
+            }
+
+            while (bA > AdjectiveList.Count())
+            {
+                bA -= AdjectiveList.Count();
+            }
+            while (bN > NounList.Count())
+            {
+                bN -= NounList.Count();
+            }
+
+            string bI = $"\n{(string)AdjectiveList[bA]} {(string)NounList[bN]}";
+
+            foreach (char c in bI) { Console.Write("-"); }
+            Console.WriteLine(bI);
+            foreach (char c in bI) { Console.Write("-"); }
+            Console.WriteLine("\n");
+            return;
+        }
+
+        public string DetermineCharacterValue(string name)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>
+            {
+                {'A', 1},
+                {'B', 4},
+                {'C', 2},
+                {'D', 3},
+                {'E', 1},
+                {'F', 5},
+                {'G', 4},
+                {'H', 5},
+                {'I', 1},
+                {'J', 7},
+                {'K', 5},
+                {'L', 2},
+                {'M', 3},
+                {'N', 2},
+                {'O', 1},
+                {'P', 3},
+                {'Q', 8},
+                {'R', 1},
+                {'S', 2},
+                {'T', 5},
+                {'U', 5},
+                {'V', 7},
+                {'W', 6},
+                {'X', 7},
+                {'Y', 6},
+                {'Z', 5},
+                {' ', 10},
+                {'.', 10},
+                {'-', 10}
+            };
+
+            int sum = name.Sum(c => map.TryGetValue(c, out int v) ? v : 0);
+            return sum.ToString();
+        }
+    }
+}
---
+++ ComplimentGeneratorConsoleApp/Program.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ComplimentGeneratorConsoleApp
+{
+    class Program
+    {
+        private static bool firstRandomModeMethodCall = true;
+
+        static void Main(string[] args)
+        {
+            InitializecomplimentGenerator();
+        }
+
+        public static void InitializecomplimentGenerator()
+        {
+            firstRandomModeMethodCall = true;
+            DisplayASCIIArtTitle();
+            Console.WriteLine("\nWelcome to compliment Generator!");
+            Console.WriteLine("\n\tType 1 to select Randomizer Mode");
+            Console.WriteLine("\tType 2 to select Name Mode");
+            Console.WriteLine("\tType 3 to select Birthday Mode");
+            Console.WriteLine("\tType 4 to exit the application");
+            Console.Write("\nPlease type 1, 2, 3, or 4 to select a mode: ");
+            string mode = Console.ReadLine();
+
+            ModeSelection(mode);
+        }
+        public static void ModeSelection(string mode)
+        {
+            string m = "";
+            int caseSwitch = 0;
+            bool success = Int32.TryParse(mode, out caseSwitch);
+            if (success)
+            {
+                switch (caseSwitch)
+                {
+                    case 1:
+                        DisplayASCIIArtTitle(caseSwitch);
+                        RandomizerMode();
+                        break;
+                    case 2:
+                        DisplayASCIIArtTitle(caseSwitch);
+                        NameMode();
+                        break;
+                    case 3:
+                        DisplayASCIIArtTitle(caseSwitch);
+                        BirthdayMode();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nGoodbye!");
+                        Thread.Sleep(500);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("\nPlease type a valid number to select a mode (1, 2, 3, or 4):");
+                        m = Console.ReadLine();
+                        ModeSelection(m);
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("\nPlease type a valid number to select a mode (1, 2, 3, or 4): ");
+                m = Console.ReadLine();
+                ModeSelection(m);
+            }
+        }
+
+        public static void RandomizerMode()
+        {
+            if (firstRandomModeMethodCall == true)
+            {
+                Console.Write("Press the ENTER key to generate a random compliment.");
+                Console.ReadLine();
+            }
+            Compliment compliment = new Compliment();
+
+            compliment.RandomCompliment();
+            Console.WriteLine("\tType 1 to generate another compliment");
+            Console.WriteLine("\tType 2 to return to mode selection");
+            Console.WriteLine("\tType 3 to exit the application");
+            Console.Write("\nPlease make a selection: ");
+            string option = Console.ReadLine();
+
+            firstRandomModeMethodCall = false;
+            RandomizerModeOptionSelection(option);
+        }
+        public static void NameMode()
+        {
+            Console.Write("\nPlease type your name (or someone else's): ");
+            string name = Console.ReadLine();
+            if (name.Length <= 0) { NameMode(); }
+            Compliment compliment = new Compliment();
+            compliment.NameCompliment(name);
+
+            Console.WriteLine("\tType 1 to type in another name");
+            Console.WriteLine("\tType 2 to return to mode selection");
+            Console.WriteLine("\tType 3 to exit the application");
+            Console.Write("\nPlease make a selection: ");
+            string option = Console.ReadLine();
+            NameModeOptionSelection(option);
+        }
+        public static void BirthdayMode()
+        {
+            Compliment compliment = new Compliment();
+
+            if (firstRandomModeMethodCall == true)
+            {
+                Console.Write("\nPlease type your birthday (or someone else's) in MM/DD/YYYY format: ");
+            }
+            string birthday = Console.ReadLine();
+            if (DateTime.TryParse(birthday, out DateTime theDate))
+            {
+                compliment.BirthdayCompliment(birthday);
+            }
+            else
+            {
+                Console.Write("Please type your date in MM/DD/YYYY format (remove any symbols around date if necessary): ");
+                firstRandomModeMethodCall = false;
+                BirthdayMode();
+            }
+
+            firstRandomModeMethodCall = true;
+            Console.WriteLine("\tType 1 to type in another name");
+            Console.WriteLine("\tType 2 to return to mode selection");
+            Console.WriteLine("\tType 3 to exit the application");
+            Console.Write("\nPlease make a selection: ");
+            string option = Console.ReadLine();
+            BirthdayModeOptionSelection(option);
+        }
+
+        public static void RandomizerModeOptionSelection(string option)
+        {
+            string o = "";
+            int caseSwitch = 0;
+            bool success = Int32.TryParse(option, out caseSwitch);
+            if (success)
+            {
+                switch (caseSwitch)
+                {
+                    case 1:
+                        RandomizerMode();
+                        break;
+                    case 2:
+                        InitializecomplimentGenerator();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nGoodbye!");
+                        Thread.Sleep(500);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                        o = Console.ReadLine();
+                        RandomizerModeOptionSelection(o);
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                o = Console.ReadLine();
+                RandomizerModeOptionSelection(o);
+            }
+        }
+        public static void NameModeOptionSelection(string option)
+        {
+            string o = "";
+            int caseSwitch = 0;
+
+            bool success = Int32.TryParse(option, out caseSwitch);
+            if (success)
+            {
+                switch (caseSwitch)
+                {
+                    case 1:
+                        NameMode();
+                        break;
+                    case 2:
+                        InitializecomplimentGenerator();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nGoodbye!");
+                        Thread.Sleep(500);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                        o = Console.ReadLine();
+                        NameModeOptionSelection(o);
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                o = Console.ReadLine();
+                NameModeOptionSelection(o);
+            }
+        }
+        public static void BirthdayModeOptionSelection(string option)
+        {
+            string o = "";
+            int caseSwitch = 0;
+
+            bool success = Int32.TryParse(option, out caseSwitch);
+            if (success)
+            {
+                switch (caseSwitch)
+                {
+                    case 1:
+                        BirthdayMode();
+                        break;
+                    case 2:
+                        InitializecomplimentGenerator();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nGoodbye!");
+                        Thread.Sleep(500);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                        o = Console.ReadLine();
+                        BirthdayModeOptionSelection(o);
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("\nPlease type a valid number to make a selection (1, 2, or 3): ");
+                o = Console.ReadLine();
+                BirthdayModeOptionSelection(o);
+            }
+        }
+
+        public static void DisplayASCIIArtTitle()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("ComplimentGeneratorTitleScreen.txt");
+                String line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                    Thread.Sleep(50);
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        public static void DisplayASCIIArtTitle(int caseSwitch)
+        {
+            Console.WriteLine("\n\n\n");
+            switch (caseSwitch)
+            {
+                case 1:
+                    try
+                    {
+                        StreamReader sr = new StreamReader("RandomizerModeMenuScreen.txt");
+                        String line = sr.ReadLine();
+                        while (line != null)
+                        {
+                            Console.WriteLine(line);
+                            line = sr.ReadLine();
+                            Thread.Sleep(50);
+                        }
+                        sr.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+                        StreamReader sr = new StreamReader("NameModeMenuScreen.txt");
+                        String line = sr.ReadLine();
+                        while (line != null)
+                        {
+                            Console.WriteLine(line);
+                            line = sr.ReadLine();
+                            Thread.Sleep(50);
+                        }
+                        sr.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        StreamReader sr = new StreamReader("BirthdayModeMenuScreen.txt");
+                        String line = sr.ReadLine();
+                        while (line != null)
+                        {
+                            Console.WriteLine(line);
+                            line = sr.ReadLine();
+                            Thread.Sleep(50);
+                        }
+                        sr.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    break;
+            }
+        }
+    }
+}
